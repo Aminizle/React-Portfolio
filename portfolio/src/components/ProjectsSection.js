0ee08c78b17ce16676f10fd1c18e5cde6de916df
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-02-10 22:33:49 -0500

    added description to first project

--- portfolio/src/components/ProjectsSection.js
+++ portfolio/src/components/ProjectsSection.js
@@ -9,7 +9,16 @@ const PastProjects = () => {
     <div className="flex flex-wrap justify-center text-white">
       <div className="w-full lg:w-1/2 p-3">
         <img src={travel} alt="Project 3" className="rounded-lg" />
-        <p className="mt-3 text-center text-sm">Project 3 Description</p>
+        <div className="grid grid-cols-2">
+        <p className="mt-3 text-center text-sm">A fully responsive travel website optimized for all screen sizes, featuring a user-friendly navigation menu, integrated social media links, and an eye catching video background.</p>
+        <div>
+        <p className="mt-3 text-center text-sm">Tech Stack:</p>
+        <ul>
+            <li>HTML 5</li>
+            <li>CSS 3</li>
+        </ul>
+        </div>
+        </div>
       </div>
       <div className="w-full lg:w-1/2 p-3">
         <img src={marvel} alt="Project 1" className="rounded-lg" />
