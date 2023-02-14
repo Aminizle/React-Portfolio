88d718d9bd70c2d09dcac4c449ef5352756f35f5
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-02-14 11:15:37 -0500

    formatting skills in progress

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -17,105 +17,106 @@ import {
 
 const Skills = () => {
   return (
-    <div className="grid md:grid-cols-2 gap-10 py-10 lg:flex-row lg:flex-wrap text-white max-width">
-      <div className=" m-3 text-left">
-        <h3 className="text-xl font-bold">Front End</h3>
-        <ul className="list-disc pl-5  ">
-          <li className="flex">
-            <SiJavascript />
-            JavaScript
-          </li>
-          <li className="flex">
-            <SiTypescript />
-            TypeScript
-          </li>
-          <li className="flex">
-            <SiHtml5 />
-            HTML
-          </li>
-          <li className="flex">
-            <SiCss3 />
-            CSS
-          </li>
-          <li className="flex">
-            <SiTailwindcss />
-            Tailwind CSS
-          </li>
-          <li className="flex">
-            <SiBootstrap />
-            Bootstrap
-          </li>
-          <li className="flex">
-            <SiReact />
-            React.js
-          </li>
-          <li className="flex">
-            <SiNextdotjs />
-            Next.js
-          </li>
-        </ul>
+    <div className="grid md:grid-cols-2 gap-10 py-10 text-white max-width ">
+      <div className="flex justify-center justify-items-center">
+        <div className=" m-3 text-left ">
+          <h3 className="text-xl font-bold">Front End</h3>
+          <ul className="list-disc pl-5  ">
+            <li className="flex">
+              <SiJavascript />
+              JavaScript
+            </li>
+            <li className="flex">
+              <SiTypescript />
+              TypeScript
+            </li>
+            <li className="flex">
+              <SiHtml5 />
+              HTML
+            </li>
+            <li className="flex">
+              <SiCss3 />
+              CSS
+            </li>
+            <li className="flex">
+              <SiTailwindcss />
+              Tailwind CSS
+            </li>
+            <li className="flex">
+              <SiBootstrap />
+              Bootstrap
+            </li>
+            <li className="flex">
+              <SiReact />
+              React.js
+            </li>
+            <li className="flex">
+              <SiNextdotjs />
+              Next.js
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className=" m-3 text-left">
-        <h3 className="text-xl font-bold">Back End</h3>
-        <ul className="list-disc pl-5">
-          <li className="flex">
-            <SiNodedotjs />
-            Node.js
-          </li>
-          <li className="flex">
-            <SiStrapi />
-            strapi
-          </li>
-          <li className="flex">
-            <SiExpress />
-            Express
-          </li>
-          <li className="flex">
-            <SiMongodb />
-            MongoDB
-          </li>
-          <li className="flex">
-            <SiPostgresql />
-            PostgreSQL
-          </li>
-        </ul>
+      <div className="flex justify-center justify-items-center">
+        <div className=" m-3 text-left">
+          <h3 className="text-xl font-bold">Back End</h3>
+          <ul className="list-disc pl-5">
+            <li className="flex">
+              <SiNodedotjs />
+              Node.js
+            </li>
+            <li className="flex">
+              <SiStrapi />
+              strapi
+            </li>
+            <li className="flex">
+              <SiExpress />
+              Express
+            </li>
+            <li className="flex">
+              <SiMongodb />
+              MongoDB
+            </li>
+            <li className="flex">
+              <SiPostgresql />
+              PostgreSQL
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className=" m-3 text-left">
-        <h3 className="text-xl font-bold">Development Tools</h3>
-        <ul className="list-disc pl-5">
-          <li>
-            Version control: Git
-          </li>
-          <li>
-            Project management: Trello, JIRA & Azure DevOps
-          </li>
-          <li>
-            Collaboration tools: Slack, Microsoft Teams, Confluence
-          </li>
-          <li>
-            integrated development environment (IDE): Visual Studio Code &
-            intellij
-          </li>
-          <li>
-            Continuous integration and deployment (CI/CD): Jenkins & Azure
-            DevOps
-          </li>
-          <li>
-            API testing: Postman
-          </li>
-        </ul>
+      <div className="flex justify-center justify-items-center">
+        <div className=" m-3 text-left">
+          <h3 className="text-xl font-bold">Development Tools</h3>
+          <ul className="list-disc pl-5">
+          
+            <li>Version control: Git</li>
+            <li>Project management: Trello, JIRA & Azure DevOps</li>
+            <li>Collaboration tools: Slack, Microsoft Teams, Confluence</li>
+            <li>
+              integrated development environment (IDE): Visual Studio Code &
+              intellij
+            </li>
+            <li>
+              Continuous integration and deployment (CI/CD): Jenkins & Azure
+              DevOps
+            </li>
+            <li>API testing: Postman</li>
+          </ul>
+        </div>
       </div>
-      <div className=" m-3 text-left">
-        <h3 className="text-xl font-bold">Professional Skills</h3>
-        <ul className="list-disc pl-5">
-          <li>Agile Scrum</li>
-          <li>kanaban</li>
-          <li>Team management</li>
-          <li>Effective communication</li>
-          <li>Adaptability</li>
-          <li>Problem-solving</li>
-          <li>Collaboration</li>
-        </ul>
+      <div className="flex justify-center justify-items-center">
+        <div className=" m-3 text-left">
+          <h3 className="text-xl font-bold">Professional Skills</h3>
+          <ul className="list-disc pl-5">
+            <li>Agile Scrum</li>
+            <li>kanaban</li>
+            <li>Team management</li>
+            <li>Effective communication</li>
+            <li>Adaptability</li>
+            <li>Problem-solving</li>
+            <li>Collaboration</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
