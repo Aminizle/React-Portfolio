20f55ab01b0dff17212df861ad7851e31f34711c
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-02-25 18:57:38 -0500

    formatting resume download button

--- portfolio/src/components/ResumeButton.js
+++ portfolio/src/components/ResumeButton.js
@@ -1,24 +1,23 @@
 import { Button } from "@material-tailwind/react";
 import React, { Component } from "react";
 import Pdf from "../assets/Resume.pdf";
-import {GrDownload} from "react-icons/gr";
+import { GrDownload } from "react-icons/gr";
 
 class OpenPdfButton extends Component {
   render() {
     return (
-      <div>
-        <Button className=" bg-cyan-500 hover:bg-teal-500 text-wide my-16 text-white px-4 py-2 rounded-md shadow-xl hover:shadow-cyan-500/50 hover:scale-110 mx-auto">
+      <div className="flex bg-cyan-500 hover:bg-teal-500 hover:shadow-cyan-500/50 hover:scale-110 shadow-xl rounded-md my-16 w-60 mx-auto justify-center">
+        <Button className="  rounded-r-none">
           <a href={Pdf} target="_blank" rel="noreferrer">
             View my Resumé
           </a>
-        <Button className=" bg-cyan-500 hover:bg-teal-500 text-wide my-16 text-white px-4 py-2 rounded-md shadow-xl hover:shadow-cyan-500/50 hover:scale-110 mx-auto">
-          <a href={Pdf} download="Pdf" target="_blank" rel="noreferrer">
-            <GrDownload/>
+        </Button>
+        <Button className=" px-4 rounded-l-none px- ">
+          <a href={Pdf} download="Pdf" rel="noreferrer">
+            <GrDownload className="text-xl text-white " />
           </a>
         </Button>
-        </Button>
       </div>
-      
     );
   }
 }
